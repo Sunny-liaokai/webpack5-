e32d407b3db5db39832c42516a4e9d516c09f030
LK_Admin <1775765534@qq.com>
2022-05-27 14:34:32 +0800

    管理资源

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports={
     entry: './src/index.js',//入口
     output: {
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname,'dist'),//出口路径
     }
 }
