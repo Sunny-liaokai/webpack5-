bbf32bfd67c51a9a1549ccf6670540a66ebbb3ca
LK_Admin <1775765534@qq.com>
2022-05-27 16:28:28 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import _ from 'lodash'
 import './style.css'
 import Icon from './Icon.jpg'
-function component(){
-    const el =document.createElement('div')
-    el.innerHTML=_.join(['Hello','webpack'],'')
+import printMe from './print'
+
+function component () {
+    const el = document.createElement('div')
+    el.innerHTML = _.join(['Hello', 'webpack'], '')
     el.classList.add('hello')
+    const btn = document.createElement('btn')
+    btn.innerHTML='点击哈哈哈哈'
+    btn.onclick = printMe;
 
-
-    const myIcon=new Image()
+    const myIcon = new Image()
     myIcon.src = Icon
 
     el.appendChild(myIcon)
+
+    el.appendChild(btn)
     return el
 }
+
 document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path')
+/*根据依引入的文件自动打包成html文件*/
+const  HtmlWebpackPlugin =require('html-webpack-plugin')
 module.exports={
-    entry: './src/index.js',//入口
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname,'dist'),//出口路径
+    entry: {
+        index:'./src/index.js',
+        print:'./src/print.js'
     },
+    output: {
+        // filename: "bundle.js",
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname,'dist'),//出口路径
+        clean:true,//每次打包前清理dist文件夹
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "管理输出"
+        })
+    ],
     module: {
         rules: [{
             test: /\.css$/i,
