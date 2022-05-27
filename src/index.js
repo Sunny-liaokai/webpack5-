4f3907024ad4086c3448dc5a53cb7f44d3e862f9
LK_Admin <1775765534@qq.com>
2022-05-27 15:28:58 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './Icon.jpg'
 function component(){
     const el =document.createElement('div')
     el.innerHTML=_.join(['Hello','webpack'],'')
+    el.classList.add('hello')
+
+
+    const myIcon=new Image()
+    myIcon.src = Icon
+
+    el.appendChild(myIcon)
     return el
 }
 document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,14 @@ module.exports={
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname,'dist'),//出口路径
+    },
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use:['style-loader','css-loader']
+        },{
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource"
+        }]
     }
 }
